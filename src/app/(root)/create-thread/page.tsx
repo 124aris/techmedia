fe0b3c908641d544bf7a75f0fa6379f51ad4dab5
Muhammad Haris <mharis01470017@gmail.com>
2024-10-03 23:13:26 +0500

    "Added user authentication and authorization checks, implemented PostThread component, and updated Home page to display threads. Also, added FormMessage component to AccountProfile and created fetchUser function in user.actions."

--- src/app/(root)/create-thread/page.tsx
+++ src/app/(root)/create-thread/page.tsx
@@ -1,7 +1,18 @@
-
+import PostThread from "@/components/PostThread";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function CreateThread() {
+    const user = await currentUser();
+    if(!user) return null;
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
     return(
-        <h1 className="head-text">Create Thread</h1>
+        <>
+            <h1 className="head-text">Create Thread</h1>
+            <PostThread userId={userInfo._id}/>
+        </>
     )
 }
--- src/components/AccountProfile.tsx
+++ src/components/AccountProfile.tsx
@@ -10,6 +10,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { z } from "zod";
@@ -128,6 +129,7 @@ export default function AccountProfile({ user, btnTitle }: Props) {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+              <FormMessage/>
             </FormItem>
           )}
         />
@@ -146,6 +148,7 @@ export default function AccountProfile({ user, btnTitle }: Props) {
                   {...field}
                 />
               </FormControl>
+              <FormMessage/>
             </FormItem>
           )}
         />
@@ -164,6 +167,7 @@ export default function AccountProfile({ user, btnTitle }: Props) {
                   {...field}
                 />
               </FormControl>
+              <FormMessage/>
             </FormItem>
           )}
         />
@@ -182,6 +186,7 @@ export default function AccountProfile({ user, btnTitle }: Props) {
                   {...field}
                 />
               </FormControl>
+              <FormMessage/>
             </FormItem>
           )}
         />
