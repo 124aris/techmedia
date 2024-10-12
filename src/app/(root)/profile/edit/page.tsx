d0ac7f04b23fa37d13c50aa7904b9b6acd3d2999
Muhammad Haris <mharis01470017@gmail.com>
2024-10-12 14:31:50 +0500

    edit page

--- src/app/(root)/profile/edit/page.tsx
+++ src/app/(root)/profile/edit/page.tsx
@@ -1,5 +1,29 @@
-export default function Edit() {
-    return(
-        <div className="flex justify-center items-center text-light-1 text-lg mt-10">Not Now Please Leave Me Alone</div>
-    )
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/AccountProfile";
+
+export default async function Edit() {
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  const userData = {
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
+
+  return (
+    <>
+      <h1 className='head-text'>Edit Profile</h1>
+      <p className='mt-3 text-base-regular text-light-2'>Make any changes</p>
+      <section className='mt-12'>
+        <AccountProfile user={userData} btnTitle='Continue' />
+      </section>
+    </>
+  );
 }
